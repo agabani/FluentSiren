a426751b7866400efa9b3b41d8cd0b41be03883c
Ahmed Agabani <agabani@live.com>
2016-06-19 03:15:58 +0100

    Updated project description

--- FluentSiren.AspNet.WebApi/Properties/AssemblyInfo.cs
+++ FluentSiren.AspNet.WebApi/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FluentSiren.AspNet.WebApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A fluent API to build specification enforced siren entities. (ASP.NET WebAPI integration)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FluentSiren.AspNet.WebApi")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.1.0")]
+[assembly: AssemblyFileVersion("0.6.1.0")]

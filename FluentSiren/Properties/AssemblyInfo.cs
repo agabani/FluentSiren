0f873c1e05e976ed0980978513156cba58177df3
Ahmed Agabani <agabani@live.com>
2016-06-19 01:10:15 +0100

    Added nuspec

--- FluentSiren/Properties/AssemblyInfo.cs
+++ FluentSiren/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FluentSiren")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A fluent API to build specification enforced siren entities.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FluentSiren")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.1.0")]
+[assembly: AssemblyFileVersion("0.6.1.0")]
